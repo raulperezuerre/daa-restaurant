011a5fd5810fbf1700f955eea8a92a7842a4677b
raulperezgzz <raul.perez.gzz@gmail.com>
2023-02-25 02:40:11 -0600

    added users component

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { UsersService } from './users.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css'],
+})
+export class UsersComponent implements OnInit {
+  users: any;
+
+  constructor(private usersService: UsersService) {}
+
+  ngOnInit() {
+    this.usersService.getUsers().subscribe((res) => {
+      this.users = res;
+      console.log(this.users);
+    });
+  }
+}
